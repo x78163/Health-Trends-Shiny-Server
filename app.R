0a7616a7fdf5e71c5be44a8da755ec021af5d22c
Joe DiFrancesco <joseph.difrancesco@gmail.com>
2018-01-10 15:53:53 +0100

    Update app.R

--- app.R
+++ app.R
@@ -13,7 +13,7 @@ library(shiny)
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Joe's Super Awesome App"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
