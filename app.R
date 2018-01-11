62c846023a7a84f9a8c4195604298eca690e6fc5
x78163 <joseph.difrancesco@gmail.com>
2018-01-11 13:55:21 +0100

    google trends problems

--- app.R
+++ app.R
@@ -6,7 +6,7 @@
 #
 #    http://shiny.rstudio.com/
 #
-if(!require(gtrendsR)) install.packages("gtrendsR",repos = "http://cran.us.r-project.org")
+#if(!require(gtrendsR)) install.packages("gtrendsR",repos = "http://cran.us.r-project.org")
 library(shiny)
 
 
